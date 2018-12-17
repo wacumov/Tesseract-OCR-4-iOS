6cea85e251ce6965a838d8156b6e8feb783f07ac
Dirk Holtwick <dirk.holtwick@gmail.com>
2018-12-17 11:42:21 +0100

    Provide a result for bitmap creation

--- TesseractOCR/G8Tesseract.mm
+++ TesseractOCR/G8Tesseract.mm
@@ -509,19 +509,24 @@ namespace tesseract {
     [self resetFlags];
 }
 
-- (void)setBitmapImageRep:(NSBitmapImageRep *)imageRep {
+- (BOOL)useBitmapImageRep:(NSBitmapImageRep *)imageRep {
     if (!imageRep) {
         NSLog(@"ERROR: No image rep");
-        return;
+        return NO;
     }
 
     self.imageSize = CGSizeMake(imageRep.pixelsWide, imageRep.pixelsHigh);
     if (self.imageSize.width <= 0 || self.imageSize.width <= 0) {
         NSLog(@"ERROR: Image rep with wrong size");
-        return;
+        return NO;
     }
 
     unsigned char *imageData = imageRep.bitmapData;
+    if (!imageData) {
+        NSLog(@"ERROR: Image rep with no imageData");
+        return NO;
+    }
+
     int bytes_per_line = (int)imageRep.bytesPerRow;
     int bytes_per_pixel = (int)imageRep.bitsPerPixel / 8;
     int bits_per_pixel = (int)(bytes_per_pixel == 0 ? 1 : bytes_per_pixel * 8);
@@ -537,6 +542,7 @@ namespace tesseract {
     _image = image;
 
     [self resetFlags];
+    return YES;
 }
 
 #endif
